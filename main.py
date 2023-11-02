3b7fdd065a51a29c658fc34472781026eef2e150
Izabela Szczecinska <izabelaszczecinska5@gmail.com>
2023-11-02 15:03:20 +0100

    added countdown mechanism and timer mechanism

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 #colorhunt.co
 PINK = "#e2979c"
@@ -7,6 +8,20 @@ GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 #
 FONT_NAME = "Courier"
+WORK_MIN = 25
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(5 * 60)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = '0' + f'{count_sec}'
+    canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,12 +39,12 @@ check_mark_label.grid(column=1, row=3)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(103, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(103, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
 
 
 #Button
-start_button = Button(text='Start', bg='white')
+start_button = Button(text='Start', bg='white', command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text='Reset', bg='white')
